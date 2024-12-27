00f84a02347b694c3c8ec63b9e686381d32e2117
Anton Silaev <silaevap2020@gmail.com>
2024-12-27 18:00:32 +0700

    finish

--- LineAdjustment.Tests/LineAdjustmentAlgorithmTests.cs
+++ LineAdjustment.Tests/LineAdjustmentAlgorithmTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 
 namespace LineAdjustment.Tests
 {
@@ -8,14 +9,13 @@ namespace LineAdjustment.Tests
         [TestCase(null, 5, "")]
         [TestCase("", 5, "")]
         [TestCase("test", 5, "test ")]
-        [TestCase("Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 12,
-            "Lorem  ipsum\ndolor    sit\namet        \nconsectetur \nadipiscing  \nelit  sed do\neiusmod     \ntempor      \nincididunt  \nut labore et\ndolore magna\naliqua      ")]
+        [TestCase("Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 12, "Lorem  ipsum\ndolor    sit\namet        \nconsectetur \nadipiscing  \nelit  sed do\neiusmod     \ntempor      \nincididunt  \nut labore et\ndolore magna\naliqua      ")]
         [TestCase("Lorem     ipsum    dolor", 17, "Lorem ipsum dolor")]
         public void Simple(string input, int lineWidth, string expected)
         {
             var algorithm = new LineAdjustmentAlgorithm();
             var output = algorithm.Transform(input, lineWidth);
-            Assert.AreEqual(expected, output);
+            output.Should().BeEquivalentTo(expected);
         }
     }
 }
--- LineAdjustment/LineAdjustmentAlgorithm.cs
+++ LineAdjustment/LineAdjustmentAlgorithm.cs
@@ -1,12 +1,92 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace LineAdjustment
+namespace LineAdjustment;
+
+public class LineAdjustmentAlgorithm
 {
-    public class LineAdjustmentAlgorithm
+    public string Transform(string input, int lineWidth)
     {
-        public string Transform(string input, int lineWidth)
+        if (string.IsNullOrEmpty(input))
         {
-            throw new NotImplementedException();
+            return string.Empty;
         }
+
+        var linesBuilder = new StringBuilder(input.Length * 2);
+
+        //Span<Range> ranges = stackalloc Range[input.Length];
+        var inputSpan = input.AsSpan();
+        // var cnt = inputSpan.Split(ranges, [' '], StringSplitOptions.RemoveEmptyEntries);
+        var ranges = inputSpan.Split(' ');
+
+        var currentLineRanges = new List<Range>(input.Length);
+        var currentLength = 0;
+
+        foreach (var range in ranges)
+        {
+            var word = inputSpan[range];
+            var wordLength = word.Length;
+            if (wordLength == 0)
+            {
+                continue;
+            }
+
+            if (currentLength + wordLength + currentLineRanges.Count <= lineWidth)
+            {
+                currentLineRanges.Add(range);
+                currentLength += wordLength;
+            }
+            else
+            {
+                var justifyLine = JustifyLine(inputSpan, currentLineRanges, lineWidth);
+
+                linesBuilder.Append(justifyLine).Append('\n');
+
+                currentLineRanges.Clear();
+                currentLineRanges.Add(range);
+                currentLength = wordLength;
+            }
+        }
+
+        var lastLine = JustifyLine(inputSpan, currentLineRanges, lineWidth);
+        linesBuilder.Append(lastLine);
+
+        return linesBuilder.ToString();
+    }
+
+    private static StringBuilder JustifyLine(ReadOnlySpan<char> src, List<Range> ranges, int lineWidth)
+    {
+        var capacity = ranges.Sum(r => r.End.Value - r.Start.Value);
+
+        var result = new StringBuilder(capacity);
+
+        if (ranges.Count == 1)
+        {
+            return result.Append(src[ranges[0]]).Append(new string(' ', lineWidth - src[ranges[0]].Length));
+        }
+
+        var totalSpaces = lineWidth - capacity;
+        var spaceSlots = ranges.Count - 1;
+
+        if (spaceSlots <= 0)
+        {
+            return result.Append(src[ranges[0]]).Append(new string(' ', totalSpaces));
+        }
+
+        var spaceBetweenWords = totalSpaces / spaceSlots;
+        var extraSpaces = totalSpaces % spaceSlots;
+
+        result.Append(src[ranges[0]]);
+
+        for (var i = 1; i < ranges.Count; i++)
+        {
+            var spaces = spaceBetweenWords + (i <= extraSpaces ? 1 : 0);
+            result.Append(new string(' ', spaces));
+            result.Append(src[ranges[i]]);
+        }
+
+        return result;
     }
 }
