ad8757f878e2569d65d75fa492ac66f674b45216
Anton Silaev <silaevap2020@gmail.com>
2025-01-12 20:12:41 +0700

    add bencmarks

---
+++ LineAdjustment.Benchmark/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using LineAdjustment;
+using Microsoft.Extensions.ObjectPool;
+
+var summary = BenchmarkRunner.Run<ToCacheOrNotToCache>();
+
+[MemoryDiagnoser]
+public class ToCacheOrNotToCache
+{
+    private const string Input = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+    private const int LineWidth = 12;
+
+    private readonly ObjectPool<StringBuilder> _stringBuilderPool;
+
+    public ToCacheOrNotToCache()
+    {
+        var objectPoolProvider = new DefaultObjectPoolProvider();
+        _stringBuilderPool = objectPoolProvider.CreateStringBuilderPool(1024, int.MaxValue);
+    }
+
+    [Benchmark]
+    public void UseCache()
+    {
+        var algorithm = new LineAdjustmentAlgorithm(_stringBuilderPool) { UseSpaceCache = true };
+        var output = algorithm.Transform(Input, LineWidth);
+    }
+
+    [Benchmark]
+    public void NotUseCache()
+    {
+        var algorithm = new LineAdjustmentAlgorithm(_stringBuilderPool) { UseSpaceCache = true };
+        var output = algorithm.Transform(Input, LineWidth);
+    }
+}
